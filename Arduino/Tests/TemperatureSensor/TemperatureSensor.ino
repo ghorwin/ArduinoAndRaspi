dae8bca84911add5796f9e60ce3699818700544f
Andreas Nicolai <A.Nicolai@uit-gmbh.de>
2024-03-06 15:29:06 +0100

    Added tests for DS18D20 temperature sensors

--- Arduino/Tests/TemperatureSensor/TemperatureSensor.ino
+++ Arduino/Tests/TemperatureSensor/TemperatureSensor.ino
@@ -11,23 +11,29 @@
 // Setup a oneWire instance to communicate with any OneWire devices
 OneWire oneWire(ONE_WIRE_BUS);
 
-// Pass our oneWire reference to Dallas Temperature sensor 
+// Pass our oneWire reference to Dallas Temperature sensor
 DallasTemperature sensors(&oneWire);
 
-void setup(void)
-{
+#define DS18B20_Aufloesung 12
+DeviceAddress DS18B20_Adressen;
+
+void setup(void) {
   // Start serial communication for debugging purposes
   Serial.begin(9600);
   // Start up the library
   sensors.begin();
-}
 
-void loop(void){ 
+  Serial.println("Anzahl aktivierter Sensoren: ");
+  Serial.println(sensors.getDeviceCount());
+ }
+
+void loop(void) {
   // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
-  sensors.requestTemperatures(); 
-  
+  sensors.requestTemperatures();
+
   Serial.print("Celsius temperature: ");
   // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
-  Serial.print(sensors.getTempCByIndex(0)); 
+  Serial.println(sensors.getTempCByIndex(0));
+
   delay(1000);
 }
