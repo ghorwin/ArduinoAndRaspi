bc512a94a72ebf5f5d20d97890229915786058ec
Andreas Nicolai <andreas.nicolai@gmx.net>
2024-09-28 14:01:15 +0200

    RGB LED animation mode

--- Arduino/Makeblock-mBot-Ranger/KnightRaiderTest/KnightRaiderTest.ino
+++ Arduino/Makeblock-mBot-Ranger/KnightRaiderTest/KnightRaiderTest.ino
@@ -1,7 +1,10 @@
 /* Testprogramm für den LED Ring auf dem Auriga Board. */
 
-// Zuerst den Header für das Auriga-Board einbinden
 #include <MeAuriga.h>
+
+// Der Auriga on-board LED Ring hat 12 LEDs. Zur Vereinfachung der Lesbarkeit
+// legen wir hier ein Define fest.
+#define LEDNUM 12
 
 /*! Immer, wenn die Funktion step() aufgerufen wird, 
     werden die letzten x LEDs etwas abgedunkelt (die letzte immer aus) 
@@ -9,55 +12,148 @@
 */
 class KnightRaiderLEDs {
 public:
+  /*! Constructor, takes optional number of trailing LEDs (max 12). */
+  KnightRaiderLEDs(uint8_t trailingLedCount = 6);
+
   void setup();
 
-  void step(bool links, uint8_t red, uint8_t green, uint8_t blue);
- 
+  /*! Alle LEDs ausschalten */
+  void turnOff();
 
-  // Der Auriga on-board LED Ring hat 12 LEDs. Zur Vereinfachung der Lesbarkeit
-  // legen wir hier ein Define fest.
-  #define LEDNUM  12
+  /*! Steps one RGB forward (either clockwise or anti-clockwise direction), sets it to the new rgb value and dims the last 6 LEDs a bit. */
+  void step(bool clockWise, uint8_t red, uint8_t green, uint8_t blue);
 
-  // Beim Auriga-Board steuern wird den on-board LED Ring an, 
+  // Beim Auriga-Board steuern wird den on-board LED Ring an,
   // welcher über Port 0 (PORT0, onboard) angesteuert wird.
   // Die Klasse MeRGBLed bietet die Schnittstellenfunktionen dafür.
-  MeRGBLed led( PORT0, LEDNUM );
+  MeRGBLed    m_led;
 
-  // Anzahl der hinterherschleifenden LEDs
-  #define TRAILING_LED_DISTANCE 6
+  uint8_t     m_trailingLedCount;
+  /*! The current (brightest) LED. */
+  uint8_t     m_currentPos = 8; // topmost LED
 
-  cRGB trailingLEDS[TRAILING_LED_DISTANCE];
+  /*! Holds info on last LED. */
+  struct LastLED {
+    cRGB rgb;
+    uint8_t index;
+  };
 
+  // holds history of last shown LEDs
+  LastLED        m_trailingLEDS[LEDNUM];
 };
 
-KnightRaiderLEDs::setup() {
+// *** Implementierung der Klasse KnightRaiderLEDs ***
+
+KnightRaiderLEDs::KnightRaiderLEDs(uint8_t trailingLedCount) : 
+  m_led(PORT0, LEDNUM)
+{
+  if (trailingLedCount > LEDNUM)
+    m_trailingLedCount = LEDNUM;
+  else 
+    m_trailingLedCount = max(1, trailingLedCount);
+}
+
+void KnightRaiderLEDs::setup() {
   // LED Ring Controller ist am PWM PIN D44 angeschlossen
-  led.setpin( 44 );
+  m_led.setpin(44);
   // Nach dem Setzen des Pin muss minimal kurz gewartet werden.
-  // Wenn man den delay()-Aufruf vergisst, dann wird nach dem 
+  // Wenn man den delay()-Aufruf vergisst, dann wird nach dem
   // led.show() Befehl die erste LED in grün angezeigt.
   delay(1);
   // nun können wir die initalen LED-Werte (alle AUS/SCHWARZ) setzen
-  led.show();
+  m_led.show();
+
+  for (int i = 0; i < m_trailingLedCount; ++i) {
+    m_led.setColorAt(i, 0,0,0); // Mind use of setColorAt() which uses zero-based indexing
+  }
 }
 
+void KnightRaiderLEDs::turnOff() {
+  m_led.setColor(0, 0, 0, 0);  // all LEDs off
+  m_led.show();
+}
 
-KnightRaiderLEDs leds;
+void KnightRaiderLEDs::step(bool clockWise, uint8_t red, uint8_t green, uint8_t blue) {
+  if (clockWise)
+    m_currentPos = (m_currentPos + 1) % LEDNUM;   // Modulo 12 ->  (11 + 1) = 12  -> 12 % 12 = 0
+  else
+    m_currentPos = (m_currentPos + LEDNUM - 1) % LEDNUM;   // Modulo 12 ->  (0 + 11) = 12  -> 11 % 12 = 11
+  
+  // shift and dim trailing leds
+  for (int i=m_trailingLedCount-1; i>0; --i) {
+    // move LEDs one position forward
+    m_trailingLEDS[i] = m_trailingLEDS[i-1];
+  }
 
+  // last trailing LED will be turned off
+  m_trailingLEDS[m_trailingLedCount-1].rgb.r = 0;
+  m_trailingLEDS[m_trailingLedCount-1].rgb.g = 0;
+  m_trailingLEDS[m_trailingLedCount-1].rgb.b = 0;
+
+  // set current led
+  m_trailingLEDS[0].rgb.r = red;
+  m_trailingLEDS[0].rgb.g = green;
+  m_trailingLEDS[0].rgb.b = blue;
+  m_trailingLEDS[0].index = m_currentPos;
+
+  // now update the colors in the LED positions, but back to front in case we have direction reversal
+  for (int i=m_trailingLedCount-1; i>=0; --i) {
+    // compute dim factor
+    // i = 0                     -> dim = 1
+    // i = m_trailingLedCount-1  -> dim = 0
+    // 
+    float dim = float(m_trailingLedCount - 1.0 - i)/(m_trailingLedCount - 1);
+    // we have to dim a bit more progressively towards low values
+    dim = dim*dim;
+    m_led.setColorAt(m_trailingLEDS[i].index, dim*m_trailingLEDS[i].rgb.r, dim*m_trailingLEDS[i].rgb.g, dim*m_trailingLEDS[i].rgb.b);
+    // Serial.print(m_trailingLEDS[i].index);
+    // Serial.print(";");
+    // Serial.print(m_trailingLEDS[i].rgb.r);
+    // Serial.print(";");
+    // Serial.print(m_trailingLEDS[i].rgb.g);
+    // Serial.print(";");
+    // Serial.println(m_trailingLEDS[i].rgb.b);
+  }
+
+  m_led.show();
+}
+
+KnightRaiderLEDs leds(9);
 
 void setup() {
   leds.setup();
+  Serial.begin(115200);
 }
 
 
 void loop() {
-  // wir setzen nun unterschiedliche Farben in den LEDs und schalten diese dann jeweils an
-  for (uint8_t i = 0; i < LEDNUM; ++i ) 
-     led.setColorAt( i, i*20, 210-i*20, i*10 ); // i = 0...11
-  led.show();
-  delay(1000);
-  // alle LED wieder ausschalten
-  led.setColor(0,0,0);
-  led.show();
-  delay(1000);
+  // 4 times knight rider
+  for (int i=0; i<12; ++i) {
+    leds.step(true, 255,0,0);
+    delay(100);
+  }
+  for (int i=0; i<12; ++i) {
+    leds.step(false, 255,0,0);
+    delay(100);
+  }
+  for (int i=0; i<12; ++i) {
+    leds.step(true, 255,0,0);
+    delay(100);
+  }
+  for (int i=0; i<12; ++i) {
+    leds.step(false, 255,0,0);
+    delay(100);
+  }
+  // now the color loop
+  for (int i=0;i<16; ++i) {
+    // color bits
+    bool r = i & 0x1;
+    bool g = i & 0x2;
+    bool b = i & 0x4;
+    for (int i=0; i<12; ++i) {
+      leds.step(true, r*255,g*255,b*255);
+      delay(100);
+    }
+
+  }
 }
